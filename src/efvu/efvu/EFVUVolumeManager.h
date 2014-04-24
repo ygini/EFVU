32593ed427888d352ea71567e8e2e68fcdde5d02
Yoann Gini <yoann.gini@gmail.com>
2014-04-24 20:34:06 +0200

    Create an interface object to handle all volume operation logic.

---
+++ src/efvu/efvu/EFVUVolumeManager.h
@@ -0,0 +1,13 @@
+//
+//  EFVUVolumeManager.h
+//  efvu
+//
+//  Created by Yoann Gini on 24/04/2014.
+//  Copyright (c) 2014 iNig-Services. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EFVUVolumeManager : NSObject
+
+@end
---
+++ src/efvu/efvu/EFVUVolumeManager.m
@@ -0,0 +1,13 @@
+//
+//  EFVUVolumeManager.m
+//  efvu
+//
+//  Created by Yoann Gini on 24/04/2014.
+//  Copyright (c) 2014 iNig-Services. All rights reserved.
+//
+
+#import "EFVUVolumeManager.h"
+
+@implementation EFVUVolumeManager
+
+@end
